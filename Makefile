e9e1c833c85840750cdef8fce9c6906a1750de69
ShrekMan293 <112657471+ShrekMan293@users.noreply.github.com>
2024-10-06 12:59:15 -0400

    Transitioning from Limine to Kernel

--- Makefile
+++ Makefile
@@ -1,16 +1,8 @@
-CFLAGS := gcc -Wall -Wextra -nostdlib -fpic -ffreestanding -fno-stack-protector \
- 			-fno-stack-check -fshort-wchar -mno-red-zone -maccumulate-outgoing-args -I./inc \
- 			--entry start -m32 -fno-builtin -O1
-
-SRCFILES := ./bootmgr/src/bootmgr.c
-BINFILE := ./bootmgr/bin/bootmgr.bin
+DRVFILES := ./drivers/src/ahci.c
+DRVBIN := ./drivers/bin/diskDriver.elf
 MESSAGE := ""
 
 ELFSRCS := ./setup/src/kmain.c
-
-gcc:
-	$(CFLAGS) $(SRCFILES) -o $(BINFILE)
-	objcopy --only-section=.text --only-section=.data --only-section=.bss -O binary $(BINFILE)
 
 #efibuild:
 #	gcc -Ignu-efi/inc -fpic -ffreestanding -fno-stack-protector -fno-stack-check -fshort-wchar -mno-red-zone -maccumulate-outgoing-args -c ./efi/src/main.c -o ./efi/obj/main.o
@@ -25,8 +17,12 @@ run:
 	-device ahci,id=ahci -device ide-hd,drive=disk,bus=ahci.0 \
 	-net none
 
-build:
-	nasm -f bin ./bootmgr/src/setup.asm -o ./bootmgr/bin/setup.bin
+run-ide:
+	qemu-system-x86_64 -display sdl -monitor stdio -cpu max -m 1G \
+  	-drive if=pflash,format=raw,unit=0,file=./test/OVMF_CODE.fd,readonly=on \
+  	-drive if=pflash,format=raw,unit=1,file=./test/OVMF_VARS.fd \
+	-drive id=disk,file=./test/disc.vhd,if=ide,format=raw \
+	-net none
 
 git:
 	git add ./
@@ -43,13 +39,25 @@ efi_reset:
 	git checkout HEAD -- ./test/OVMF_VARS.fd
 
 liminebuild:
-	x86_64-elf-gcc -Isetup/inc -ffreestanding -fno-stack-protector -fno-stack-check -nostdlib -nostartfiles -T./setup/linker.ld -Wall -Wextra $(ELFSRCS) -o ./setup/bin/setup.elf
+	x86_64-elf-gcc -I./inc -ffreestanding -fno-stack-protector -fno-stack-check -nostdlib -nostartfiles -lgcc -T./setup/linker.ld -Wall -Wextra $(ELFSRCS) -o ./setup/bin/setup.elf
 
+limine_wsl: liminebuild
+	copy /b .\setup\bin\setup.elf "\\wsl.localhost\Ubuntu\mnt\esp\limine\yutboo"
+	copy /b .\test\limine.conf "\\wsl.localhost\Ubuntu\mnt\esp\limine\limine.conf"
 limine: liminebuild
-	copy /b .\setup\bin\setup.elf E:\limine\yutboo\\
+	copy /b .\setup\bin\setup.elf "E:\limine\yutboo\setup.elf"
+	copy /b .\test\limine.conf "E:\limine\limine.conf"
 
-asm: build write run
-efi: liminebuild run
+driver:
+	x86_64-elf-gcc -I./inc -I./drivers/inc -ffreestanding -fpic -fno-stack-protector -fno-stack-check -nostdlib -nostartfiles -lgcc -T./drivers/linker.ld -Wall -Wextra $(DRVFILES) -o $(DRVBIN)
+
+build_wsl: limine_wsl driver
+	copy /b .\drivers\bin\diskDriver.elf "\\wsl.localhost\Ubuntu\mnt\esp\limine\yutboo"
+
+build: limine driver
+	copy /b .\drivers\bin\diskDriver.elf "E:\limine\yutboo\diskDriver.elf"
+
+all: build run
 
 #	ld -shared -Bsymbolic -Lgnu-efi/x86_64/lib -Lgnu-efi/x86_64/gnuefi -Tgnu-efi/gnuefi/elf_x86_64_efi.lds gnu-efi/x86_64/gnuefi/crt0-efi-x86_64.o ./efi/obj/main.o -o ./efi/obj/main.so -lgnuefi -lefi
 #	objcopy -j .text -j .sdata -j .data -j .rodata -j .dynamic -j .dynsym  -j .rel -j .rela -j .rel.* -j .rela.* -j .reloc --target efi-app-x86_64 --subsystem=10 ./efi/obj/main.so ./efi/bin/BOOTX64.efi
--- inc/fis.h
+++ inc/fis.h
@@ -1,4 +1,7 @@
-#include "inttypes.h"
+#ifndef YUTBOO_FIS_H
+#define YUTBOO_FIS_H
+
+#include "int.h"
 
 typedef enum
 {
@@ -15,150 +18,152 @@ typedef enum
 typedef struct tagFIS_REG_H2D
 {
 	// DWORD 0
-	uint8_t  fis_type;	// FIS_TYPE_REG_H2D
+	u8  fis_type;	// FIS_TYPE_REG_H2D
  
-	uint8_t  pmport:4;	// Port multiplier
-	uint8_t  rsv0:3;		// Reserved
-	uint8_t  c:1;		// 1: Command, 0: Control
+	u8  pmport:4;	// Port multiplier
+	u8  rsv0:3;		// Reserved
+	u8  c:1;		// 1: Command, 0: Control
  
-	uint8_t  command;	// Command register
-	uint8_t  featurel;	// Feature register, 7:0
+	u8  command;	// Command register
+	u8  featurel;	// Feature register, 7:0
  
 	// DWORD 1
-	uint8_t  lba0;		// LBA low register, 7:0
-	uint8_t  lba1;		// LBA mid register, 15:8
-	uint8_t  lba2;		// LBA high register, 23:16
-	uint8_t  device;		// Device register
+	u8  lba0;		// LBA low register, 7:0
+	u8  lba1;		// LBA mid register, 15:8
+	u8  lba2;		// LBA high register, 23:16
+	u8  device;		// Device register
  
 	// DWORD 2
-	uint8_t  lba3;		// LBA register, 31:24
-	uint8_t  lba4;		// LBA register, 39:32
-	uint8_t  lba5;		// LBA register, 47:40
-	uint8_t  featureh;	// Feature register, 15:8
+	u8  lba3;		// LBA register, 31:24
+	u8  lba4;		// LBA register, 39:32
+	u8  lba5;		// LBA register, 47:40
+	u8  featureh;	// Feature register, 15:8
  
 	// DWORD 3
-	uint8_t  countl;		// Count register, 7:0
-	uint8_t  counth;		// Count register, 15:8
-	uint8_t  icc;		// Isochronous command completion
-	uint8_t  control;	// Control register
+	u8  countl;		// Count register, 7:0
+	u8  counth;		// Count register, 15:8
+	u8  icc;		// Isochronous command completion
+	u8  control;	// Control register
  
 	// DWORD 4
-	uint8_t  rsv1[4];	// Reserved
+	u8  rsv1[4];	// Reserved
 } FIS_REG_H2D;
 
 typedef struct tagFIS_REG_D2H
 {
 	// DWORD 0
-	uint8_t  fis_type;    // FIS_TYPE_REG_D2H
+	u8  fis_type;    // FIS_TYPE_REG_D2H
  
-	uint8_t  pmport:4;    // Port multiplier
-	uint8_t  rsv0:2;      // Reserved
-	uint8_t  i:1;         // Interrupt bit
-	uint8_t  rsv1:1;      // Reserved
+	u8  pmport:4;    // Port multiplier
+	u8  rsv0:2;      // Reserved
+	u8  i:1;         // Interrupt bit
+	u8  rsv1:1;      // Reserved
  
-	uint8_t  status;      // Status register
-	uint8_t  error;       // Error register
+	u8  status;      // Status register
+	u8  error;       // Error register
  
 	// DWORD 1
-	uint8_t  lba0;        // LBA low register, 7:0
-	uint8_t  lba1;        // LBA mid register, 15:8
-	uint8_t  lba2;        // LBA high register, 23:16
-	uint8_t  device;      // Device register
+	u8  lba0;        // LBA low register, 7:0
+	u8  lba1;        // LBA mid register, 15:8
+	u8  lba2;        // LBA high register, 23:16
+	u8  device;      // Device register
  
 	// DWORD 2
-	uint8_t  lba3;        // LBA register, 31:24
-	uint8_t  lba4;        // LBA register, 39:32
-	uint8_t  lba5;        // LBA register, 47:40
-	uint8_t  rsv2;        // Reserved
+	u8  lba3;        // LBA register, 31:24
+	u8  lba4;        // LBA register, 39:32
+	u8  lba5;        // LBA register, 47:40
+	u8  rsv2;        // Reserved
  
 	// DWORD 3
-	uint8_t  countl;      // Count register, 7:0
-	uint8_t  counth;      // Count register, 15:8
-	uint8_t  rsv3[2];     // Reserved
+	u8  countl;      // Count register, 7:0
+	u8  counth;      // Count register, 15:8
+	u8  rsv3[2];     // Reserved
  
 	// DWORD 4
-	uint8_t  rsv4[4];     // Reserved
+	u8  rsv4[4];     // Reserved
 } FIS_REG_D2H;
 
 typedef struct tagFIS_DATA
 {
 	// DWORD 0
-	uint8_t  fis_type;	// FIS_TYPE_DATA
+	u8  fis_type;	// FIS_TYPE_DATA
  
-	uint8_t  pmport:4;	// Port multiplier
-	uint8_t  rsv0:4;		// Reserved
+	u8  pmport:4;	// Port multiplier
+	u8  rsv0:4;		// Reserved
  
-	uint8_t  rsv1[2];	// Reserved
+	u8  rsv1[2];	// Reserved
  
 	// DWORD 1 ~ N
-	uint32_t data[1];	// Payload
+	u32 data[1];	// Payload
 } FIS_DATA;
 
 typedef struct tagFIS_PIO_SETUP
 {
 	// DWORD 0
-	uint8_t  fis_type;	// FIS_TYPE_PIO_SETUP
+	u8  fis_type;	// FIS_TYPE_PIO_SETUP
  
-	uint8_t  pmport:4;	// Port multiplier
-	uint8_t  rsv0:1;		// Reserved
-	uint8_t  d:1;		// Data transfer direction, 1 - device to host
-	uint8_t  i:1;		// Interrupt bit
-	uint8_t  rsv1:1;
+	u8  pmport:4;	// Port multiplier
+	u8  rsv0:1;		// Reserved
+	u8  d:1;		// Data transfer direction, 1 - device to host
+	u8  i:1;		// Interrupt bit
+	u8  rsv1:1;
  
-	uint8_t  status;		// Status register
-	uint8_t  error;		// Error register
+	u8  status;		// Status register
+	u8  error;		// Error register
  
 	// DWORD 1
-	uint8_t  lba0;		// LBA low register, 7:0
-	uint8_t  lba1;		// LBA mid register, 15:8
-	uint8_t  lba2;		// LBA high register, 23:16
-	uint8_t  device;		// Device register
+	u8  lba0;		// LBA low register, 7:0
+	u8  lba1;		// LBA mid register, 15:8
+	u8  lba2;		// LBA high register, 23:16
+	u8  device;		// Device register
  
 	// DWORD 2
-	uint8_t  lba3;		// LBA register, 31:24
-	uint8_t  lba4;		// LBA register, 39:32
-	uint8_t  lba5;		// LBA register, 47:40
-	uint8_t  rsv2;		// Reserved
+	u8  lba3;		// LBA register, 31:24
+	u8  lba4;		// LBA register, 39:32
+	u8  lba5;		// LBA register, 47:40
+	u8  rsv2;		// Reserved
  
 	// DWORD 3
-	uint8_t  countl;		// Count register, 7:0
-	uint8_t  counth;		// Count register, 15:8
-	uint8_t  rsv3;		// Reserved
-	uint8_t  e_status;	// New value of status register
+	u8  countl;		// Count register, 7:0
+	u8  counth;		// Count register, 15:8
+	u8  rsv3;		// Reserved
+	u8  e_status;	// New value of status register
  
 	// DWORD 4
-	uint16_t tc;		// Transfer count
-	uint8_t  rsv4[2];	// Reserved
+	u16 tc;		// Transfer count
+	u8  rsv4[2];	// Reserved
 } FIS_PIO_SETUP;
 
 typedef struct tagFIS_DMA_SETUP
 {
 	// DWORD 0
-	uint8_t  fis_type;	// FIS_TYPE_DMA_SETUP
+	u8  fis_type;	// FIS_TYPE_DMA_SETUP
  
-	uint8_t  pmport:4;	// Port multiplier
-	uint8_t  rsv0:1;		// Reserved
-	uint8_t  d:1;		// Data transfer direction, 1 - device to host
-	uint8_t  i:1;		// Interrupt bit
-	uint8_t  a:1;            // Auto-activate. Specifies if DMA Activate FIS is needed
+	u8  pmport:4;	// Port multiplier
+	u8  rsv0:1;		// Reserved
+	u8  d:1;		// Data transfer direction, 1 - device to host
+	u8  i:1;		// Interrupt bit
+	u8  a:1;            // Auto-activate. Specifies if DMA Activate FIS is needed
  
-        uint8_t  rsved[2];       // Reserved
+        u8  rsved[2];       // Reserved
  
 	//DWORD 1&2
  
-        uint64_t DMAbufferID;    // DMA Buffer Identifier. Used to Identify DMA buffer in host memory.
+        u64 DMAbufferID;    // DMA Buffer Identifier. Used to Identify DMA buffer in host memory.
                                  // SATA Spec says host specific and not in Spec. Trying AHCI spec might work.
  
         //DWORD 3
-        uint32_t rsvd;           //More reserved
+        u32 rsvd;           //More reserved
  
         //DWORD 4
-        uint32_t DMAbufOffset;   //Byte offset into buffer. First 2 bits must be 0
+        u32 DMAbufOffset;   //Byte offset into buffer. First 2 bits must be 0
  
         //DWORD 5
-        uint32_t TransferCount;  //Number of bytes to transfer. Bit 0 must be 0
+        u32 TransferCount;  //Number of bytes to transfer. Bit 0 must be 0
  
         //DWORD 6
-        uint32_t resvd;          //Reserved
+        u32 resvd;          //Reserved
  
 } FIS_DMA_SETUP;
+
+#endif YUTBOO_FIS_H
